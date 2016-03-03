6b728979152cdeafa60427703a4bff927d7d46bc
ThinerZQ <601097836@qq.com>
2016-03-04 00:37:54 +0800

    qq

---
+++ src/main/java/com/thinerzq/leetcode/algorithms/RemoveElement_27.java
@@ -0,0 +1,21 @@
+package com.thinerzq.leetcode.algorithms;
+
+/**
+ * Created by root on 16-3-3.
+ */
+public class RemoveElement_27 {
+    public static void main(String[] args) {
+
+    }
+    public int removeElement(int[] nums, int val) {
+        int count =0;
+        for (int i=0;i<nums.length;i++){
+            if(nums[i] == val){
+                count++;
+            }else{
+                nums[i-count] = nums[i];
+            }
+        }
+        return nums.length -count;
+    }
+}
