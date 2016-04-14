fae0f6ed58356d410928b5c9f49422bc893dcdac
ThinerZQ <601097836@qq.com>
2016-04-14 22:28:20 +0800

    commit

---
+++ src/main/java/com/thinerzq/leetcode/algorithms/CountingBits_338.java
@@ -0,0 +1,31 @@
+package com.thinerzq.leetcode.algorithms;
+
+/**
+ * Created with IntelliJ IDEA
+ * Date: 2016/3/28
+ * Time: 23:05
+ * User: ThinerZQ
+ * GitHub: <a>https://github.com/ThinerZQ</a>
+ * Blog: <a>http://www.thinerzq.me</a>
+ * Email: 601097836@qq.com
+ */
+
+public class CountingBits_338 {
+    public static void main(String[] args) {
+
+    }
+    public int[] countBits(int num) {
+
+        int result[] = new int[num+1];
+        result[0]=0;
+        for (int i=1;i<=num;i++){
+            int count=0;
+            while (i!=0){
+                i&=(i-1);
+                count++;
+            }
+            result[i]=count;
+        }
+        return result;
+    }
+}
---
+++ src/main/java/com/thinerzq/leetcode/algorithms/ReverseInteger_7.java
@@ -0,0 +1,42 @@
+package com.thinerzq.leetcode.algorithms;
+
+/**
+ * Created with IntelliJ IDEA
+ * Date: 2016/3/28
+ * Time: 23:27
+ * User: ThinerZQ
+ * GitHub: <a>https://github.com/ThinerZQ</a>
+ * Blog: <a>http://www.thinerzq.me</a>
+ * Email: 601097836@qq.com
+ */
+
+public class ReverseInteger_7 {
+    public static void main(String[] args) {
+
+        reverse(-34563);
+    }
+    public static int reverse(int x) {
+
+        if (x>=0) {
+            StringBuilder stringBuilder = new StringBuilder(String.valueOf(x));
+            stringBuilder = stringBuilder.reverse();
+
+            long n = Long.parseLong(stringBuilder.toString());
+            if (n>Integer.MAX_VALUE){
+                return 0;
+            }else{
+                return (int)n;
+            }
+        }else{
+            StringBuilder stringBuilder = new StringBuilder(String.valueOf(x).substring(1));
+            stringBuilder = stringBuilder.reverse();
+
+            long n = Long.parseLong(stringBuilder.toString());
+            if (n>Integer.MAX_VALUE){
+                return 0;
+            }else{
+                return (int)-n;
+            }
+        }
+    }
+}
